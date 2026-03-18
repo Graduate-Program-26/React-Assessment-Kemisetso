8441a5edd28d462bec96299b091e23dfbfe26a8d
kemi-p <kpole@dvtsoftware.com>
2026-03-18 21:31:19 +0200

    fixed unused heroUI provider that was causing erros

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,16 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { HeroUIProvider } from "@heroui/react";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Github Dashboard",
@@ -15,11 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <HeroUIProvider>
-      <body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         {children}
       </body>
-      </HeroUIProvider>
     </html>
   );
 }
