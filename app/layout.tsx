bfdb1766eeee47efdb06a2ae62e26456994ec3e6
kemi-p <kpole@dvtsoftware.com>
2026-03-20 09:43:32 +0200

    fix conflicting versions of tailwinf, add basic styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { HeroUIProvider } from "@heroui/react";
+import Providers from "./providers";
 
 export const metadata: Metadata = {
   title: "Github Dashboard",
@@ -24,11 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+        <Providers>
         {children}
+        </Providers>
       </body>
+
     </html>
   );
 }
